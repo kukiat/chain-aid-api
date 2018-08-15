0636692140eada4cecc5b531ef803627a19fb223
kukiat <kukiat1996@gmail.com>
2018-08-15 22:39:59 +0700

    Fix env

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/api', routes)
 
-const PORT = process.event.PORT || 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log('server start lew ja')
 })
